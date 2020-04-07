3b9b5ffff2caa339c434bf20a924c39cb2af5619
Hubway <davidhoho05@gmail.com>
2020-04-07 22:51:26 +0800

    calc average review length

--- read.py
+++ read.py
@@ -1,9 +1,17 @@
 data = []
+data_length = []
 count = 0
+data_length_count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		data_length.append(len(line))
 		count += 1
-		if count % 10000 == 0:
+		data_length_count += len(line)
+		if count % 100000 == 0:
 			print(len(data))
-print(len(data))
+			print(data_length_count)
+average_length = data_length_count/len(data)
+print(data_length_count)
+print('留言平均長度為', average_length, '個字')
+print('檔案讀取完了，總共有', len(data), ' 筆資料')
