80d0ad48f85f501b189aa30bb13ccbbd3027684a
Hubway <davidhoho05@gmail.com>
2020-04-23 19:41:51 +0800

    add word count function

--- read.py
+++ read.py
@@ -1,24 +1,60 @@
+import time
+import progressbar
+
 data = []
-data_length = []
 count = 0
-data_length_count = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		data_length.append(len(line))
 		count += 1
-		data_length_count += len(line)
-		if count % 100000 == 0:
-			print(len(data))
-			print(data_length_count)
-average_length = data_length_count/len(data)
-print(data_length_count)
-print('留言平均長度為', average_length, '個字')
-print('檔案讀取完了，總共有', len(data), ' 筆資料')
+print('檔案讀取完了,總共有', len(data), '筆資料')
+
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len / len(data))
+
 
 new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print('總共有', len(new), '筆留言長度小於100')
+print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
+print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+
+
+# 文字計數
+wc = {}
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
+
+while True:
+	word = input('請問你想查甚麼字: ')
+	if word == 'q':
+		break
+	if word in wc:		
+		print(word, '出現過的次數為: ', wc[word])
+	else:
+		print('這個字沒有出現過喔!')
+
+print('感謝使用本查詢功能')
